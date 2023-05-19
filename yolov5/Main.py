3cade3794b67010445a86c22083029adefaa7df7
shigon255 <ray5233512@gmail.com>
2023-05-19 21:04:20 +0800

    yolov5 add threading feature

--- yolov5/Main.py
+++ yolov5/Main.py
@@ -4,49 +4,75 @@ import time
 from FPSDetect import *
 from ctypes import *
 from utils.FPSUtils import *
+import win32api
+import threading
+import os
 import cv2
 
+VK_W = 0x57
+
+pressed = False
+keyboard_terminate = threading.Event()
+btc = None
+
+def monitor_keyboard():
+    global pressed
+    global keyboard_terminate
+    global btc
+    while not keyboard_terminate.is_set():
+        w_key_state = win32api.GetKeyState(VK_W)
+        pressed = w_key_state < 0
+        if btc is not None and w_key_state < 0:# if the left mouse button is pressed
+            print("Coordinate: ", int(LEFT + btc[0]), int(TOP + btc[1]))
+            pyautogui.moveTo(int(LEFT + btc[0]), int(TOP + btc[1]))
+    print("keyboard interrupt in keyboard thread")
 
 def shoot_screen():
     while True:
         img = pyautogui.screenshot(region=[LEFT, TOP, 640, 640])  # take screenshot, input: (left, top, w, h)
-        # path to save screenshots
         images_path = 'images/'
         img.save(
             images_path + str(int(time.time())) + ''.join(
                 random.sample('zyxwvutsrqponmlkjihgfedcba', 2)) + '.jpg')  # generate file name randomly
         time.sleep(0.5)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    print("Initialize")
+    keyboard_thread = threading.Thread(target=monitor_keyboard)
+    keyboard_thread.start()
+    try:
+        while True:
+            try:
+                print("\n----------------------")
+                # Take screen shot
+                t = time.time()
+                print("Start taking screen shot")
+                img = ScreenShout() 
+                print("End taking screen shot, it took " + str(time.time()-t) + "s")
 
-    while True:
-        try:
-            print("\n----------------------")
-            # Take screen shot
-            t = time.time()
-            print("Start taking screen shot")
-            img = ScreenShout() 
-            print("End taking screen shot, it took " + str(time.time()-t) + "s")
+                # Detection
+                t = time.time()
+                print("Start detection")
+                detections = detect(img)
+                print("End detection, it took " + str(time.time()-t) + "s")
+                print("detection: ")
+                print(detections)
 
-            # Detection
-            t = time.time()
-            print("Start detection")
-            detections = detect(img)
-            print("End detection, it took " + str(time.time()-t) + "s")
-            print("detection: ")
-            print(detections)
+                # Find center to move
+                t = time.time()
+                print("Start finding")
+                btc, btp = FindBestCenter(detections)
+                print("End finding, it took " + str(time.time() - t) + "s")
 
-            # Find center to move
-            t = time.time()
-            print("Start finding & moving")
-            btc, btp = FindBestCenter(detections)
-            if btc is not None:
-              print("Coordinate: ", int(LEFT + btc[0]), int(TOP + btc[1]))
-              pyautogui.moveTo(int(LEFT + btc[0]), int(TOP + btc[1]))
-            print("End findmoving & moving, it took " + str(time.time() - t) + "s")
-            print("----------------------\n")
-            
-        except Exception as e:
-            print('ERROR!')
-            print("Message: ", str(e))
-            break
+                print("----------------------\n")
+            except:
+                break
+        raise
+    except Exception as e:
+        print('ERROR!')
+        print('Error: '+ str(e))
+        keyboard_terminate.set()
+        print("keyboard interrupt in main thread")
+
+    keyboard_thread.join()
+    print("program finish")
